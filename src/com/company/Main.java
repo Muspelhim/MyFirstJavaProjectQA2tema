dc1deffea06fc46fd5dbf95b19e21b5eb9c1a89a
Razvan Bustiuc <bustiucr@gmail.com>
2022-03-17 20:59:33 +0200

    Message

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+
+        String authorName = "Ionut";
+
+        System.out.println(authorName+" a plecat la piata de strada baritiu nr 9, ca sa cumpere  3 kg de mere.\nPe drum,"+authorName+" s-a intalnit cu Matilda.\nImpreuna cu Matilda au mers la piata,iar "+authorName+" a impartit cele 3 kg de mere cu Matilda");
+
+        int x = 2;
+        x+=x+=x++ + ++x;
+        System.out.println(x);
+
+
+        double y = 2.4;
+
+        long z = 12314L;
+        float a = 2.4F;
+
+        int myVariable = x + 3;
+
+        double result = x + z;
+        System.out.println(result);
+
+        System.out.println(x + z);
+
+        System.out.println("Valarea lui x este: " + x);
+        System.out.println("Rezultatul adunarii este: " + (x + z));
+        
+
+    }
+
+
+}
