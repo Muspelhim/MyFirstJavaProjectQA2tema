08346c27766595f01da488a8c685f56bbe8ba812
Razvan Bustiuc <bustiucr@gmail.com>
2022-03-23 17:26:30 +0200

    Message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,34 +2,80 @@ package com.company;
 
 public class Main {
 
+//    public static void main(String[] args) {
+//	// write your code here
+//        System.out.println("Hello world!");
+//
+//        String authorName = "Ionut";
+//
+//        System.out.println(authorName+" a plecat la piata de strada baritiu nr 9, ca sa cumpere  3 kg de mere.\nPe drum,"+authorName+" s-a intalnit cu Matilda.\nImpreuna cu Matilda au mers la piata,iar "+authorName+" a impartit cele 3 kg de mere cu Matilda");
+//
+//        int x = 2;
+//        x+=x+=x++ + ++x;
+//        System.out.println(x);
+//
+//
+//        double y = 2.4;
+//
+//        long z = 12314L;
+//        float a = 2.4F;
+//
+//        int myVariable = x + 3;
+//
+//        double result = x + z;
+//        System.out.println(result);
+//
+//        System.out.println(x + z);
+//
+//        System.out.println("Valarea lui x este: " + x);
+//        System.out.println("Rezultatul adunarii este: " + (x + z));
+//
+//        System.out.println("Hello World");
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world!");
 
-        String authorName = "Ionut";
-
-        System.out.println(authorName+" a plecat la piata de strada baritiu nr 9, ca sa cumpere  3 kg de mere.\nPe drum,"+authorName+" s-a intalnit cu Matilda.\nImpreuna cu Matilda au mers la piata,iar "+authorName+" a impartit cele 3 kg de mere cu Matilda");
-
+        System.out.println("Hello");
+        System.out.println("Razvan");
         int x = 2;
-        x+=x+=x++ + ++x;
-        System.out.println(x);
+        int y = 4;
+        int sum = x + y;
 
+        System.out.println("Suma este: " + sum);
+        int z = 9;
+        int c = 3 ;
+        int div = z / c;
 
-        double y = 2.4;
+        System.out.println("Rezultatul este: " + div);
 
-        long z = 12314L;
-        float a = 2.4F;
+        int a = -5;
+        int b = 8;
+        int d = 6;
+        int multiplicate = a + b * d;
 
-        int myVariable = x + 3;
+        System.out.println("Rezultatul este: " + multiplicate);
 
-        double result = x + z;
-        System.out.println(result);
+        float e = 55;
+        float r = 9;
+        float t = 100;
+        float percent = ( e + r ) * r  / t;
 
-        System.out.println(x + z);
+        System.out.println("Rezultatul este: " + percent);
 
-        System.out.println("Valarea lui x este: " + x);
-        System.out.println("Rezultatul adunarii este: " + (x + z));
-        
+        float p = 20;
+        float u = -3;
+        float i = 5;
+        float o = 8;
+        float result = p + u * i / o;
+
+        System.out.println("Rezultatul este: " + result);
+
+        float s = 5;
+        float f = 15;
+        float g = 3;
+        float h = 2;
+        float j = 8;
+        float r2 = s + f / g * h - g / t;
+
+        System.out.println("Rezultatul este " + r2);
 
     }
 
